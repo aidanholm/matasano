2e2d58b43c5964c8cc708700e42a3501b032409a
Aidan Holm <aidanholm@gmail.com>
2015-02-23 00:53:17 +0800

    Expose crack_single_byte_xor, score_english

--- 1.3/single_byte_xor.nim
+++ 1.3/single_byte_xor.nim
@@ -1,6 +1,6 @@
 from "../util/hex" import raw_from_hex
 
-proc score_english(buf: string): float =
+proc score_english *(buf: string): float =
     var score : float = 0;
     for i in countup(0, len(buf)-1):
         case buf[i]:
@@ -18,29 +18,32 @@ proc score_english(buf: string): float =
             else:   score += 0.0
     return score/(float)len(buf)
 
-proc buffer_xor_byte(buf_a: string, byte: char): string =
+proc buffer_xor_byte *(buf_a: string, byte: char): string =
     var result = ""
     for i in countup(0, len(buf_a)-1):
         add(result, chr(ord(buf_a[i]) xor ord(byte)))
     return result
 
-var line_a = raw_from_hex("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+proc crack_single_byte_xor *(buf: string): tuple[ result: string, key: char, score: float ] =
+    var best_result : string
+    var key : char
+    var best_score = 0.0
 
-var best_score = 0.0
-var second_best_score : float
-var key : char
-var best_result : string
+    for i in countup(0, 255):
+        var byte = chr(i)
+        var result = buffer_xor_byte(buf, byte)
+        var score = score_english(result)
+        if score > best_score:
+            key = byte
+            best_score = score
+            best_result = result
 
-for i in countup(0, 255):
-    var byte = chr(i)
-    var result = buffer_xor_byte(line_a, byte)
-    var score = score_english(result)
-    if score > best_score:
-        second_best_score = best_score
-        key = byte
-        best_score = score
-        best_result = result
+    return (result: best_result, key: key, score: best_score)
 
-echo("Answer: ", best_result)
-echo("Score : ", best_score, " (runner up = ", second_best_score, ")")
-echo("Key   : ", key)
+when isMainModule:
+    var input = raw_from_hex("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+    var result = crack_single_byte_xor(input)
+
+    echo("Answer: ", result.result)
+    echo("Score : ", result.score)
+    echo("Key   : ", result.key)
