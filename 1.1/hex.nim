56116daf827b6c0185f080a5585c3d75d070afaf
Aidan Holm <aidanholm@gmail.com>
2015-02-22 23:44:59 +0800

    Initial commit of Set 1 / Challenge 2

--- 1.1/hex.nim
+++ 1.1/hex.nim
@@ -17,3 +17,11 @@ proc raw_from_hex(hex: string): string =
             else: return nil
         add(result, chr(decimal))
     return result
+
+proc hex_from_raw(raw: string): string =
+    var result = ""
+    for i in countup(0, len(raw)-1):
+        var chars = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+        add(result, chars[ord(raw[i]) div 16])
+        add(result, chars[ord(raw[i]) mod 16])
+    return result
---
+++ 1.2/buffer_xor.nim
@@ -0,0 +1,19 @@
+include ../1.1/hex
+
+proc buffer_xor(buf_a: string, buf_b: string): string =
+    var result = ""
+    if len(buf_a) != len(buf_b):
+        return nil
+    for i in countup(0, len(buf_a)-1):
+        add(result, chr(ord(buf_a[i]) xor ord(buf_b[i])))
+    return result
+
+var line_a = raw_from_hex(readLine(stdin))
+var line_b = raw_from_hex(readLine(stdin))
+
+if line_a == nil or line_b == nil:
+    stderr.writeln("Error: input is not hexadecimal")
+elif len(line_a) != len(line_b):
+    stderr.writeln("Error: input has differing lengths")
+else:
+    echo(hex_from_raw(buffer_xor(line_a, line_b)))
