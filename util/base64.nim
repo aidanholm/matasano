e1db37d1a8223102360060767235f9e5a9c6adc4
Aidan Holm <aidanholm@gmail.com>
2015-02-23 16:26:35 +0800

    Implement raw_from_base64()

--- util/base64.nim
+++ util/base64.nim
@@ -1,4 +1,4 @@
-proc base64_char *(num: int): char =
+proc base64_char_from_int *(num: int): char =
     return case num:
         of  0..25: chr(num+ord('A'))
         of 26..51: chr(num-26+ord('a'))
@@ -7,6 +7,16 @@ proc base64_char *(num: int): char =
         of 63: '/'
         else:
             chr(0)
+
+proc int_from_base64_char(ch: char): int =
+    return case ch:
+        of  'A'..'Z': ord(ch)-ord('A')
+        of 'a'..'z': ord(ch)-ord('a')+26
+        of '0'..'9': ord(ch)-ord('0')+52
+        of '+': 62
+        of '/': 63
+        else:
+            0
 
 proc base64_from_raw *(raw: string): string =
     var result = ""
@@ -19,14 +29,33 @@ proc base64_from_raw *(raw: string): string =
 
         var value = bytes[0] shl 16 or bytes[1] shl 8 or bytes[2]
 
-        add(result, base64_char(value shr 18))
-        add(result, base64_char(value shr 12 and 0x3f))
+        add(result, base64_char_from_int(value shr 18))
+        add(result, base64_char_from_int(value shr 12 and 0x3f))
         if len(raw) - i >= 2:
-            add(result, base64_char(value shr 6 and 0x3f))
+            add(result, base64_char_from_int(value shr 6 and 0x3f))
         else:
             add(result, "=")
         if len(raw) - i >= 3:
-            add(result, base64_char(value shr 0 and 0x3f))
+            add(result, base64_char_from_int(value shr 0 and 0x3f))
         else:
             add(result, "=")
+    return result
+
+proc raw_from_base64 *(b64: string): string =
+    var result = ""
+    var i = 0
+    while i < len(b64)-3:
+        if b64[i] == "\n"[0] or b64[i] == "\r"[0]:
+            i += 1
+            continue
+
+        var q = [int_from_base64_char(b64[i]), int_from_base64_char(b64[i+1]),
+        int_from_base64_char(b64[i+2]), int_from_base64_char(b64[i+3])]
+        var value = q[0] shl 18 or q[1] shl 12 or q[2] shl 6 or q[3]
+        var triple = [value shr 16, value shr 8 and 0xff, value and 0xff]
+        result.add(chr(triple[0]))
+        result.add(chr(triple[1]))
+        result.add(chr(triple[2]))
+        i += 4
+
     return result
